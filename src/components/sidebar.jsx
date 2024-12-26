7ae291727a2f00c7f39604be46153ee0022aa2db
hydemary <hydemaryalzbayne@gmail.com>
2024-12-26 16:56:31 +0800

    feat: add sidebar component

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarHeader from "./sidebar-header";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-[#262626] w-48 h-screen">
+      <SidebarHeader />
+    </div>
+  );
+};
+
+export default Sidebar;
