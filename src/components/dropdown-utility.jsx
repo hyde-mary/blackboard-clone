f59a9c0618d0ab1afd726f1b139eeadba3fdeae0
hydemary <hydemaryalzbayne@gmail.com>
2024-12-26 20:04:31 +0800

    feat/style: Active item for Dropdown and add few styles

--- src/components/dropdown-utility.jsx
+++ src/components/dropdown-utility.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import DropdownUtilityItem from "./dropdown-utility-item";
 import { ChevronDown } from "lucide-react";
 
-const DropdownUtility = ({ placeholder, items }) => {
+const DropdownUtility = ({ title, placeholder, items }) => {
   return (
     <div className="dropdown w-full bg-[#ffffff] relative">
       <div
+        className="absolute -top-2 left-2 px-1 text-xs font-medium text-gray-600 pr-4"
+        style={{
+          background: `linear-gradient(to bottom, #f8f8f8 50%, #ffffff 50%)`,
+        }}
+      >
+        {title}
+      </div>
+      <div
         tabIndex={0}
         role="button"
-        className="p-2 border border-gray-400 rounded-md cursor-pointer flex items-center justify-between"
+        className="p-2 border border-gray-400 text-[#262626] rounded-md cursor-pointer flex items-center justify-between"
       >
         {placeholder}
         <ChevronDown className="w-4 h-4 text-gray-600 ml-2" />
@@ -18,7 +26,11 @@ const DropdownUtility = ({ placeholder, items }) => {
         className="dropdown-content menu bg-[#ffffff] border-2 border-gray-200 z-10 w-full p-2 shadow-md absolute top-full left-0 mt-2"
       >
         {items.map((item, index) => (
-          <DropdownUtilityItem key={index} label={item.label} />
+          <DropdownUtilityItem
+            key={index}
+            label={item.label}
+            isActive={index === 0}
+          />
         ))}
       </ul>
     </div>
